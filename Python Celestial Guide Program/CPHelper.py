acd7231cee6648369099c0b2d14d8488ac685a85
jcacela0298 <132624484+jcacela0298@users.noreply.github.com>
2023-11-19 20:12:58 -0500

    Add files via upload

---
+++ Python Celestial Guide Program/CPHelper.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python
+
+# Assignment 4
+# Jack Cacela and Spencer Marks
+# This Python program is a helper program for the Celestial_Guide.py file.
+
+
+import sys
+
+
+
+def january():
+    print('''
+Your constellation is Gemini. Your traits are as follows:
+Duality: Gemini is often symbolized by the Twins, Castor and Pollux. This duality represents the trait of having multiple facets or aspects to one's personality.
+
+Versatility: The Twins in Gemini are known for their adaptability and versatility. People associated with Gemini are versatile and capable of handling various tasks and roles.
+
+Curiosity: Gemini is ruled by Mercury in astrology, which is associated with communication and curiosity. People with a Gemini influence are curious, inquisitive, and open to new experiences.
+
+Communication: Gemini is often linked to effective communication. Those influenced by Gemini may excel in verbal and written communication and are skilled at conveying ideas.
+
+Playfulness: The Twins in Gemini can also symbolize a playful and youthful nature. People with a Gemini influence have a light-hearted and fun-loving approach to life.
+    ''')
+
+def february():
+    print('''
+Your constellation is Canis Major. Your traits are as follows:
+Loyalty: Dogs, including the mythical hound associated with Canis Major, are often seen as symbols of loyalty and faithfulness. People associated with Canis Major value loyalty in their relationships and commitments.
+
+Guidance: Sirius was used as a navigational tool by ancient seafarers and travelers. This symbolizes the idea of providing guidance or being a guiding light in someone's life.
+
+Vigilance: Dogs are known for their vigilance and alertness. Individuals linked to Canis Major possess traits of vigilance, watchfulness, and a keen sense of awareness.
+
+Courage: Dogs are often associated with bravery and courage. Those associated with Canis Major are courageous individuals who face challenges head-on.
+
+Friendship: Dogs are known for their social nature and their ability to form strong bonds with humans. Canis Major symbolizes the importance of friendship, companionship, and building meaningful relationships.
+    ''')
+
+def march():
+    print('''
+Your constellation is Leo the Lion. Your traits are as follows:
+Leadership: Leo is often associated with the lion, which is seen as a symbol of leadership. People associated with Leo exhibit strong leadership qualities and a natural ability to lead others.
+
+Courage: The lion is known for its courage and bravery. Leo individuals possess the trait of fearlessness and the willingness to face challenges head-on.
+
+Confidence: Lions are confident animals, and individuals with Leo traits display a high level of self-confidence and self-assuredness.
+
+Creativity: Leo is ruled by the Sun in astrology, which is associated with creativity and self-expression. People with Leo influences have a strong creative streak and a passion for artistic endeavors.
+
+Generosity: Lions are often seen as generous and protective of their pride. Leo individuals have a generous and caring nature, particularly towards their loved ones.
+    ''')
+
+def april():
+    print('''
+Your constellation is Ursa Major. Your traits are as follows:
+Strength: The Great Bear is often seen as a symbol of physical strength. People associated with Ursa Major are perceived as strong and resilient, both physically and emotionally.
+
+Guidance: The Big Dipper, which is part of Ursa Major, has been used as a navigational tool for centuries. Those connected to Ursa Major symbolize guidance, serving as a guiding force or mentor to others.
+
+Endurance: Bears are known for their ability to endure harsh conditions and hibernate during the winter. This symbolizes endurance, patience, and the ability to weather life's challenges.
+
+Protection: In some cultures, the Great Bear has been associated with protective spirits or deities. Those linked to Ursa Major are seen as protective and caring individuals who look out for the well-being of others.
+
+Adaptability: Ursa Major's position in the night sky changes as the Earth orbits the Sun. This constant adaptation to changing perspectives symbolizes adaptability and the ability to adjust to new situations.
+    ''')
+
+def may():
+    print('''
+Your constellation is Bootes the Herdsman. Your traits are as follows:
+Responsibility: As a herdsman, Bootes is responsible for tending to and guiding the herds. People associated with Bootes are responsible individuals who take their duties and obligations seriously.
+
+Patience: Tending to herds requires patience and diligence. Those connected to Bootes exhibit the trait of patience and the ability to handle tasks that require perseverance.
+
+Steadfastness: The Herdsman's role involves consistency and reliability. Bootes individuals are known for their steadfastness and dependability in various aspects of life.
+
+Practicality: In herding, practical knowledge and skills are crucial. People with Bootes traits are practical thinkers and problem solvers, focusing on pragmatic solutions.
+
+Caring: A herdsman's role also involves care and concern for the well-being of the herds. Those associated with Bootes are caring and compassionate individuals who look out for the welfare of others.
+    ''')
+
+def june():
+    print('''
+Your constellation is Cygnus the Swan. Your traits are as follows:
+Elegance: The swan is often seen as a symbol of elegance and grace. People associated with Cygnus possess a sense of refinement and poise in their demeanor and actions.
+
+Creativity: The graceful and artistic appearance of the swan symbolize creativity. Those connected to Cygnus have a strong inclination toward creative pursuits such as art, music, or dance.
+
+Freedom: Swans are known for their ability to glide gracefully across bodies of water. Cygnus individuals value freedom and independence in their lives and seek opportunities for personal expression.
+
+Transformation: In some cultures, the swan is associated with transformation and rebirth. Those with Cygnus traits undergo significant personal growth and transformation in their lives.
+
+Sensitivity: The swan is a sensitive and intuitive creature. People associated with Cygnus have heightened sensitivity, empathy, and an ability to connect with the emotions and feelings of others.
+    ''')
+
+def july():
+    print('''
+Your constellation is Lyra the Harp. Your traits are as follows:
+Harmony: The harp is a musical instrument associated with harmony and balance. People associated with Lyra strive for harmony in their relationships and surroundings.
+
+Artistry: Playing the harp requires skill and artistry. Those connected to Lyra are creative individuals with a talent for artistic expression, such as music, writing, or visual arts.
+
+Expression: The harp is a means of emotional expression. People with Lyra traits have a strong desire to express their thoughts, feelings, and creativity in meaningful ways.
+
+Serenity: The music of the harp is often soothing and serene. Lyra individuals embody a sense of inner peace and tranquility, as well as the ability to bring calm to others.
+
+Spirituality: In various cultures, the harp has been associated with spirituality and connection to the divine. Those associated with Lyra have a strong spiritual or philosophical outlook on life.
+    ''')
+
+def august():
+    print('''
+Your constellation is Aquila the Eagle. Your traits are as follows:
+Vision: Eagles are known for their keen eyesight and sharp vision. People associated with Aquila possess a visionary outlook, the ability to see the bigger picture, and a forward-thinking mindset.
+
+Courage: Eagles are symbols of courage and boldness. Those connected to Aquila exhibit bravery and the willingness to take risks to achieve their goals.
+
+Freedom: Eagles are often seen as symbols of freedom and independence. Aquila individuals value personal freedom and autonomy in their lives.
+
+Leadership: The eagle's majestic presence can symbolize leadership qualities. People with Aquila traits have natural leadership abilities and the capacity to inspire and guide others.
+
+Determination: Eagles are known for their persistence and determination in pursuing their prey. Those associated with Aquila possess strong determination and a never-give-up attitude in the face of challenges.
+    ''')
+
+def september():
+    print('''
+Your constellation is Pegasus the Winged Horse. Your traits are as follows:
+Freedom: Pegasus is a symbol of freedom and the ability to soar above limitations. People associated with Pegasus value personal freedom and the pursuit of their dreams without constraint.
+
+Imagination: The image of a winged horse encourages creativity and imagination. Those connected to Pegasus have a vivid and imaginative mind, often using their creativity to find innovative solutions.
+
+Adventure: Pegasus is often associated with epic adventures in Greek mythology. Individuals with Pegasus traits have a natural inclination toward adventure, exploration, and seeking new experiences.
+
+Inspiration: Pegasus was a source of inspiration in mythology, often inspiring poets and artists. People associated with Pegasus have the ability to inspire others through their actions, art, or ideas.
+
+Grace: The image of a graceful winged horse symbolizes elegance and poise. Those with Pegasus traits possess a graceful and refined demeanor, both physically and in their interactions with others.
+    ''')
+
+def october():
+    print('''
+Your constellation is Andromeda the Princess. Your traits are as follows:
+Courage: In Greek mythology, Andromeda displayed great courage in facing challenges and her eventual rescue by Perseus. People associated with Andromeda possess courage in the face of adversity.
+
+Resilience: Andromeda endured difficult circumstances and emerged stronger. Those connected to Andromeda exhibit resilience, the ability to bounce back from setbacks, and adaptability.
+
+Independence: The story of Andromeda's liberation can symbolize independence. Individuals with Andromeda traits value their independence and the ability to make their own choices.
+
+Compassion: Andromeda's compassion was evident in her willingness to sacrifice herself to save her people. People associated with Andromeda have a caring and compassionate nature.
+
+Perseverance: Andromeda's determination to overcome challenges represents perseverance. Those with Andromeda traits display perseverance in their pursuits and a strong work ethic.
+    ''')
+
+def november():
+    print('''
+Your constellation is Perseus the Hero. Your traits are as follows:
+Courage: Perseus is celebrated for his heroic deeds and the courage he displayed in facing formidable challenges. People associated with Perseus possess great courage in the face of adversity.
+
+Resourcefulness: Perseus used his wit and resourcefulness to overcome obstacles and defeat monsters. Those connected to Perseus exhibit resourcefulness and creative problem-solving skills.
+
+Compassion: Perseus showed compassion in his quest to save Andromeda and in other acts of heroism. Individuals with Perseus traits have a caring and compassionate nature.
+
+Determination: Perseus demonstrated unwavering determination in his quests. People associated with Perseus display determination, persistence, and a strong will to achieve their goals.
+
+Leadership: Perseus often led and inspired others in his adventures. Those with Perseus traits possess leadership qualities and the ability to guide and motivate others.
+    ''')
+
+def december():
+    print('''
+Your constellation is Orion the Hunter. Your traits are as follows:
+Bravery: Orion is celebrated for his bravery as a legendary hunter. People associated with Orion exhibit courage and fearlessness in various aspects of their lives.
+
+Determination: Orion's determination in pursuing his prey symbolizes a strong will and perseverance. Those connected to Orion possess determination and the ability to stay focused on their goals.
+
+Self-Reliance: As a skilled hunter, Orion was self-reliant and self-sufficient. Individuals with Orion traits value independence and self-reliance in their pursuits.
+
+Leadership: Orion's prominent position in the night sky can symbolize leadership qualities. People associated with Orion have natural leadership abilities and the capacity to guide and inspire others.
+
+Adventure: Orion's many adventures, including encounters with mythical creatures like Scorpius, represent an adventurous spirit and a willingness to explore the unknown.
+    ''')
