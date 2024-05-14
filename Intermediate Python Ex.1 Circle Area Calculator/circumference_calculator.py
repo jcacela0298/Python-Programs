07503d5b19720b94cae4fb500d19773910248855
jcacela0298 <132624484+jcacela0298@users.noreply.github.com>
2024-05-13 21:09:02 -0400

    Update circumference_calculator.py

--- Intermediate Python Ex.1 Circle Area Calculator/circumference_calculator.py
+++ Intermediate Python Ex.1 Circle Area Calculator/circumference_calculator.py
@@ -1,5 +1,14 @@
 # This is my first Python program from the intermediate Python course
 # Looking forward to seeing how my code improves as the class progresses
 
-radius = int(input("Enter a radius: ")) 
-print("The circumference is", 2 * radius * 3.14) 
+
+
+
+# Prompt the user for the radius
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the circumference
+circumference = 2 * radius * 3.14
+
+# Print the result
+print("The circumference of the circle is:", circumference)
