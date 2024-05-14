9a184f69333cd0dbcfff57fde48efae11ec6e327
Jack Cacela <jackcacela5@gmail.com>
2024-05-13 20:51:26 -0400

    Adding first intermediate Python script

---
+++ Intermediate Python Ex.1 Circle Area Calculator/circumference_calculator.py
@@ -0,0 +1,6 @@
+# This is my first Python program from the intermediate Python course
+# Looking forward to seeing how my code improves as the class progresses
+
+radius = int(input("Enter a radius: ")) 
+print("The circumference is", 2 * radius * 3.14) 
+ 
