066ff7d6c019edcae04179505005c9ec0c8e9320
Jack Cacela <jackcacela5@gmail.com>
2024-07-25 09:43:24 -0400

    update linked lists.py file with comments

--- Data Structures/linked_lists.py
+++ Data Structures/linked_lists.py
@@ -2,10 +2,15 @@ class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
+	# When you create a node like node1 = Node(data="banana"), node1 holds the data "banana" and has its next attribute initially set to None.
+
+
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.head = None # Address reference (not data value) to the first node, which is blank when a linked list is first created.
+	# The Node class doesn't need self.head because each node is only responsible for storing its own data and the reference to the next node. The LinkedList class, on the other hand, manages the entire sequence of nodes, so it needs self.head to keep track of the first node.
+
 
     def print(self):
         if self.head is None:
@@ -18,6 +23,8 @@ class LinkedList:
             itr = itr.next
         print(llstr)
 
+
+
     def get_length(self):
         count = 0
         itr = self.head
@@ -27,13 +34,18 @@ class LinkedList:
 
         return count
 
-    def insert_at_begining(self, data):
-        node = Node(data, self.head)
-        self.head = node
+
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head) # Create a new node, and makes the new node's "next" value equal to the original self.head, which is paving the way so we can create a new self.head value below with:
+
+        self.head = node 
+
+
 
     def insert_at_end(self, data):
         if self.head is None:
-            self.head = Node(data, None)
+            self.head = Node(data, None) # Set "next" == None because it is the last node.
             return
 
         itr = self.head
@@ -41,14 +53,16 @@ class LinkedList:
         while itr.next:
             itr = itr.next
 
-        itr.next = Node(data, None)
+        itr.next = Node(data, None) # Sets the "next" value of the current last node to equal the new node that will take the place of the final node, and this final node will now have "None" as the "next" value.
+
+
 
     def insert_at(self, index, data):
         if index<0 or index>self.get_length():
             raise Exception("Invalid Index")
 
         if index==0:
-            self.insert_at_begining(data)
+            self.insert_at_beginning(data)
             return
 
         count = 0
@@ -61,6 +75,8 @@ class LinkedList:
 
             itr = itr.next
             count += 1
+
+
 
     def remove_at(self, index):
         if index<0 or index>=self.get_length():
@@ -80,10 +96,14 @@ class LinkedList:
             itr = itr.next
             count+=1
 
+
+
     def insert_values(self, data_list):
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
+
+
 
     def insert_after_value(self, data_after, data_to_insert):
         if self.head is None:
@@ -101,6 +121,8 @@ class LinkedList:
 
             itr = itr.next
 
+
+
     def remove_by_value(self, data):
         if self.head is None:
             return
@@ -115,6 +137,8 @@ class LinkedList:
                 itr.next = itr.next.next
                 break
             itr = itr.next
+
+
 
 if __name__ == '__main__':
     ll = LinkedList()
